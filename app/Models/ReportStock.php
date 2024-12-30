3638a0f7aa5c5470ee7acbb35c7e45060328fb1c
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-12-30 15:19:47 +0800

    fix model for stock report

--- app/Models/ReportStock.php
+++ app/Models/ReportStock.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReportStock extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'vessel_id',
+        'request_by',
+        'project',
+        'nomor_po_logistik',
+        'deskripsi',
+        'jenis_tipe',
+        'quantity',
+        'uom',
+        'tanggal_request',
+        'actual_tiba',
+        'actual_quantity',
+        'actual_uom',
+        'nomor_po_berkah',
+        'stock_status',
+        'foto_barang_diterima',
+        'quantity_supply',
+        'uom_supply',
+        'nomor_po_supply',
+        'status_stock_supply',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function vessel()
+    {
+        return $this->belongsTo(Vessel::class);
+    }
 }
--- database/migrations/2024_12_30_035151_create_report_stocks_table.php
+++ database/migrations/2024_12_30_035151_create_report_stocks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('report_stocks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained();
             $table->foreignId('vessel_id')->constrained();
             $table->string('request_by');
             $table->string('project');
